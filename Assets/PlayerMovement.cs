05df9c162b3412656b395e4d3a01c6e104f35a87
by-bai <baihe.soh@gmail.com>
2021-06-11 00:45:36 +0800

    tilemaps and misobee jump

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Hello, world!"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("update");
+        if (Input.GetKey("space"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0); 
+        }
+    }
+}
