fb187be4777447b6cfebf40f4f38710b082cd188
by-bai <baihe.soh@gmail.com>
2021-06-12 15:49:13 +0800

    flip misobee, create CameraController
    # camera controller follows misobee without rotating
    # use of SerializeField to expose C# variables in Unity

---
+++ Assets/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    //To follow Player, need a reference to the Player's Transform which tells us the position of the Player
+    //Player is a different Component, so cannot use GetComponent
+    //Therefore, serialise the field so that we can drag Player to the script in Unity.
+
+    [SerializeField] private Transform player;
+
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        //follow player but
+        //transform.position.z -> keep the z value
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,6 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
+    private SpriteRenderer sprite;
+
+    private float dirX = 0f;
+    //SerializeField allows editing in Unity Editor
+    [SerializeField] private float moveSpeed = 7f; 
+    [SerializeField] private float jumpForce = 14f; 
 
     //int wholeNumber = 16;
     //float decimalNumber = 4.54f;
@@ -18,7 +24,8 @@ public class PlayerMovement : MonoBehaviour
         //Debug.Log("Hello, world!");
 
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>(); 
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>(); 
     }
 
     // Update is called once per frame
@@ -26,27 +33,35 @@ public class PlayerMovement : MonoBehaviour
     {
         //Debug.Log("update");
 
-        float dirX = Input.GetAxisRaw("Horizontal"); //direction X axis
+        dirX = Input.GetAxisRaw("Horizontal"); //direction X axis
         // press left -1, press right +1. joystick will have values in between
         // GetAxisRaw goes back to 0 after releasing immediately, GetAxis doesn't
 
-        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y); //horizontal and vertical
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y); //horizontal and vertical
             // vector 2 for 2D game, vector 3 for 3D game
             // multiply because if dirX is -1, velocity will be negative and move left. cleaner than using if/else
             // multiplying also provide joystick support. if dirX = -0.5 then move by -3.5
             // rb.velocity.y instead of 0 --> the rigidbody value it has the frame before
         if (Input.GetButtonDown("Jump"))
         {
-           rb.velocity = new Vector2(rb.velocity.x, 14f); 
+           rb.velocity = new Vector2(rb.velocity.x, jumpForce); 
         }
 
+        UpdateAnimationState();
+        
+    }
+
+    private void UpdateAnimationState()
+    {
         if (dirX > 0f)
         {
             anim.SetBool("running", true);
+            sprite.flipX = false; 
         }
         else if (dirX < 0f)
         {
             anim.SetBool("running", true);
+            sprite.flipX = true; 
         }
         else
         {
