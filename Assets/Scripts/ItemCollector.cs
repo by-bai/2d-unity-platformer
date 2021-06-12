8a8828597af32b4f3f488c047b6b39295296de0d
by-bai <baihe.soh@gmail.com>
2021-06-12 17:49:21 +0800

    add text to UI, strawberry item, ItemCollector
    # add PressStart2P font
    # add BoxCollider and Script for Strawberry item
    # Strawberry prefab & tag

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class ItemCollector : MonoBehaviour
+{
+    private int strawberries = 0;
+
+    [SerializeField] private Text strawberriesText; 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+
+    {
+        //use this because set Box Collider of Strawberry to IsTrigger
+        //google : how to collect items in Unity
+
+        if (collision.gameObject.CompareTag("Strawberry"))
+        {
+            Destroy(collision.gameObject);
+            strawberries++;
+            strawberriesText.text = "Strawberries: " + strawberries; 
+
+        }
+    }
+}
