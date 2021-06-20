5dc3f77446002708ac2d3c59b98dd696b3827f74
by-bai <baihe.soh@gmail.com>
2021-06-20 23:21:11 +0800

    add moving platform (incomplete)

---
+++ Assets/Scripts/StickyPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Misobee")
+        {
+            collision.gameObject.transform.SetParent(transform); 
+            //set player as child of moving platform
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Misobee")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+
+}
---
+++ Assets/Scripts/WaypointFollower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollower : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] waypoints;
+    private int currentWaypointIndex = 0;
+
+    [SerializeField] private float speed = 2f;
+
+    private void Update()
+    {
+
+        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
+        {
+            //current waypoint + moving platform 
+            // <.1f = almost touching
+            currentWaypointIndex++; 
+
+            if (currentWaypointIndex >= waypoints.Length) //at the last waypoint
+            {
+                currentWaypointIndex = 0; //restart
+            }
+
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime* speed);
+        //(!) what does Time.deltaTime mean? it's very important, rmb to read up on this
+
+    }
+}
+
